28d4dd3dae9fb17937459f873d8459175ea4cd58
Fran Moreno <fran.moreno.se@gmail.com>
2023-03-01 15:03:55 +0100

    please

--- database.py
+++ database.py
@@ -15,6 +15,5 @@ def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM jobs"))
         result_all = result.all()
-        print(result_all)
-        print(os.environ.get('DB_ACCESS_STRING'))
-        return [dict(row) for row in result_all]
+        print("TYPE -------- ", type(result_all[0]))
+        return [dict(row._mapping) for row in result_all]
