b178455ffd696f76c2cddf81a60235067b94b99c
Fran Moreno <fran.moreno.se@gmail.com>
2023-03-01 14:44:04 +0100

    again

--- database.py
+++ database.py
@@ -14,4 +14,7 @@ engine = create_engine(db_connection_string, connect_args=connect_args)
 def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM jobs"))
+        print(result.all())
+        print(result.all()[0])
+        print(dict(result.all()[0]))
         return [dict(row) for row in result.all()]
