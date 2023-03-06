42721d60b14082bbf4212f1feba254c14dc7338a
Fran Moreno <fran.moreno.se@gmail.com>
2023-03-06 11:06:02 +0100

    Load applications to api page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-from database import load_jobs_from_db, load_job_from_db, add_application_to_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db, load_applications_from_db
 
 app = Flask(__name__)
 
@@ -15,6 +15,11 @@ def hello_world():
 @app.route("/api/jobs")
 def list_jobs():
     return jsonify(load_jobs_from_db())
+
+
+@app.route("/api/applications")
+def list_applications():
+    return jsonify(load_applications_from_db())
 
 
 @app.route("/job/<id>")
--- database.py
+++ database.py
@@ -20,7 +20,6 @@ def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM jobs"))
         result_all = result.all()
-        print("TYPE -------- ", type(result_all[0]))
         return [dict(row._mapping) for row in result_all]
     
 
@@ -29,6 +28,13 @@ def load_job_from_db(id):
         result = conn.execute(text(f"SELECT * FROM jobs WHERE id = {id}"))
         rows = result.all()
         return dict(rows[0]._mapping) if len(rows) >= 0 else None
+    
+
+def load_applications_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM applications"))
+        result_all = result.all()
+        return [dict(row._mapping) for row in result_all]
     
 
 def add_application_to_db(job_id, data):
